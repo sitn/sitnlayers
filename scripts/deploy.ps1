e7fdb88a1cf38e38be9f2cedc9d5ecc418c94383
maltaesousa <stephane.maltaesousa@ne.ch>
2018-12-03 14:19:41 +0100

    deploy

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,5 +1,5 @@
 $out = "\\nesitn7\webcontent\sitnlayers"
 xcopy .\index.html $out /D /Y
-xcopy .\js $out /D /Y
-xcopy .\css $out /D /Y
-xcopy .\assets $out /D /Y
+xcopy .\js $out /D /Y /I
+xcopy .\css $out /D /Y /I
+xcopy .\assets $out /D /Y /I
